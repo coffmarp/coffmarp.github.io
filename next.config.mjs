8620f13a2116297ed68a1b0da2d0a77b9f813896
Ryan Coffman <ryan@someai.com>
2022-09-10 19:06:52 -0500

    Update website using tailwind layout (#1)
    * init
    
    * try gh-pages deploy
    
    * remove is prod flag
    
    * add export action
    
    * fix next config
    
    * remove asset prefix
    
    * asset prefix to repo name
    
    * limit release to master
    
    * name and email updates

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  pageExtensions: ['jsx', 'mdx'],
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: isProd ? "/coffmarp.github.io/" : "",
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    newNextLinkBehavior: true,
+    scrollRestoration: true,
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
